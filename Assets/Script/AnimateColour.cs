7e46f52891f36b98f821b2fb4e5a3cc2f2a904d9
superaverage123 <jacobbo@student.ubc.ca>
2024-09-22 19:03:00 -0700

    Colour change script added

--- Assets/Script/AnimateColour.cs
+++ Assets/Script/AnimateColour.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimateColour : MonoBehaviour
+using UnityEngine;
+
+public class AnimateTexture : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float scrollSpeed = 0.5f;
+    private Renderer objectRenderer;
+    private Vector2 savedOffset;
+
     void Start()
     {
-        
+        objectRenderer = GetComponent<Renderer>();
+        savedOffset = objectRenderer.material.mainTextureOffset;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Animate the texture offset over time for a moving effect
+        float offsetX = Time.time * scrollSpeed;
+        float offsetY = Time.time * scrollSpeed * 0.5f; // Different speed for Y-axis
+        Vector2 newOffset = new Vector2(offsetX, offsetY);
+
+        objectRenderer.material.mainTextureOffset = newOffset;
+    }
+
+    void OnDisable()
+    {
+        // Reset the texture offset when the script is disabled
+        objectRenderer.material.mainTextureOffset = savedOffset;
     }
 }
